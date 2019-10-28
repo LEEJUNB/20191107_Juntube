4ca336db4a6585bb46777f5a8232b82609f0e921
junblee <topguide82@gmail.com>
2019-10-28 15:33:28 +0900

    create app.js && init.js

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 
 const app = express();
-const PORT = 4000;
 
-const handleListening = () => console.log(`Listening on : http://localhost:${PORT}`);
+
 const home = (req,res) => res.send("This is our home");
 const profile = (req,res) => res.send("Welcome Profile page");
 const category = (req,res) => res.send("Category Page");
@@ -32,4 +31,5 @@ app.get('/',betweenRegion,home);
 app.use(betweenMiddle);
 app.get('/profile',profile);
 app.get('/category',category);
-app.listen(`${PORT}`,handleListening);
+
+export default app;
