4265b041df2b8d942c28ba8ef0166e7318d75184
junblee <topguide82@gmail.com>
2019-10-28 16:15:02 +0900

    first express.Router() : userRouter. export router.js and app.js is import router.js

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 import express from "express";
+import morgan from "morgan"; 
 import helmet from "helmet";
-import morgan from "morgan";
-import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import { userRouter } from "./router"; // why {}? becuz particular router is called
 
 const app = express();
 
-
-const home = (req,res) => res.send("This is our home");
-const profile = (req,res) => res.send("Welcome Profile page");
+const Home = (req,res) => res.send("god Home!"); // 마지막 함수이기에 next키가 없다.
+const profile = (req,res) => res.send("What'up man");
 const category = (req,res) => res.send("Category Page");
 const betweenMiddle = (req,res,next) => {
-    res.send("intercept");
+    console.log("***** intercept *****");
     next();
 }
 const betweenRegion = (req,res,next) => {
@@ -20,16 +20,16 @@ const betweenRegion = (req,res,next) => {
     next();
 }
 
-// globally middleWare
-app.use(helmet());
+app.use(cookieParser()); 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 app.use(morgan("combined"));
-app.use(cookieParser()); // cookie is saved user_inf
-app.use(bodyParser.json()); // form
-app.use(bodyParser.urlencoded()); // form
+app.use(helmet());
 
-app.get('/',betweenRegion,home);
+app.get('/',betweenRegion,Home);
 app.use(betweenMiddle);
-app.get('/profile',profile);
+app.get("/profile",profile);
 app.get('/category',category);
+app.use("/user", userRouter); // userRouter has a URL associated with the user.
 
 export default app;
