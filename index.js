e428dceff9117a6a59ef717209dac66686b95cdd
junblee <topguide82@gmail.com>
2019-10-28 14:32:07 +0900

    babel&&nodemon

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const express = require('express');
+import express from "express";
 const app = express();
 const PORT = 4000;
 
 const handleListening = () => console.log(`Listening on : http://localhost:${PORT}`);
-const home = (req,res) => res.send("This is our home")
+const home = (req,res) => res.send("This is our home");
+const profile = (req,res) => res.send("Welcome Profile page");
+const category = (req,res) => res.send("Category Page");
 
 app.get('/',home);
+app.get('/profile',profile);
+app.get('/category',category);
 app.listen(`${PORT}`,handleListening);
