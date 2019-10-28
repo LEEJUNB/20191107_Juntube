ecd71ff847b9ed443ab402c51217aef1c67e6c21
junblee <topguide82@gmail.com>
2019-10-28 16:42:46 +0900

    make the router folder && route file

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 
 const app = express();
 
@@ -23,6 +26,8 @@ app.use(morgan("combined"));
 app.use(helmet());
 
 app.use(betweenMiddle);
+app.use("/",globalRouter); // global Router root("/")가 될 것.
 app.use("/user", userRouter); // userRouter has a URL associated with the user.
+app.use("/video", videoRouter);
 
 export default app;
