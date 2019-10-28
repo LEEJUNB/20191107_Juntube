957b1c15e871a4fef1eccc0186eddc1bcbdd3c2e
junblee <topguide82@gmail.com>
2019-10-28 15:23:07 +0900

    helmet,morgan,bodyparser,cookieparser

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
 const app = express();
 const PORT = 4000;
 
@@ -6,7 +11,7 @@ const handleListening = () => console.log(`Listening on : http://localhost:${POR
 const home = (req,res) => res.send("This is our home");
 const profile = (req,res) => res.send("Welcome Profile page");
 const category = (req,res) => res.send("Category Page");
-const between = (req,res,next) => {
+const betweenMiddle = (req,res,next) => {
     res.send("intercept");
     next();
 }
@@ -15,8 +20,16 @@ const betweenRegion = (req,res,next) => {
     console.log("Region!!");
     next();
 }
+
+// globally middleWare
+app.use(helmet());
+app.use(morgan("combined"));
+app.use(cookieParser()); // cookie is saved user_inf
+app.use(bodyParser.json()); // form
+app.use(bodyParser.urlencoded()); // form
+
 app.get('/',betweenRegion,home);
-app.use(between);
+app.use(betweenMiddle);
 app.get('/profile',profile);
 app.get('/category',category);
 app.listen(`${PORT}`,handleListening);
