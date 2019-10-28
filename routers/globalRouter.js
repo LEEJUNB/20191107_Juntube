f0fc1e2f2a9af14bfe49030ea70739c400d4292e
junblee <topguide82@gmail.com>
2019-10-28 16:57:52 +0900

    change Router.js file that is globalRouter.js

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,4 +1,12 @@
 import express from "express";
+import routes from "../routes";
+
 const globalRouter = express.Router();
+
+globalRouter.get(routes.home, (req,res) => res.send("Hooome!!!"));
+globalRouter.get(routes.join, (req,res) => res.send("Jooooin!!"));
+globalRouter.get(routes.login, (req,res) => res.send("Loooogin!!"));
+globalRouter.get(routes.logout,(req,res) => res.send("Loooogout!!!"));
+globalRouter.get(routes.search,(req,res) => res.send("Seeearch!!!"));
 
 export default globalRouter;
