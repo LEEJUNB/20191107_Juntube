d621b01da71998b68a2a9d6df25d0e97d584f7d1
junblee <topguide82@gmail.com>
2019-10-28 15:00:23 +0900

    betweenMiddleWare

--- index.js
+++ index.js
@@ -6,8 +6,17 @@ const handleListening = () => console.log(`Listening on : http://localhost:${POR
 const home = (req,res) => res.send("This is our home");
 const profile = (req,res) => res.send("Welcome Profile page");
 const category = (req,res) => res.send("Category Page");
-
-app.get('/',home);
+const between = (req,res,next) => {
+    res.send("intercept");
+    next();
+}
+const betweenRegion = (req,res,next) => {
+    res.send("res~Region");
+    console.log("Region!!");
+    next();
+}
+app.get('/',betweenRegion,home);
+app.use(between);
 app.get('/profile',profile);
 app.get('/category',category);
 app.listen(`${PORT}`,handleListening);
