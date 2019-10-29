ac7e84b001e480a8f543ea4b26b81858f0993b29
junblee <topguide82@gmail.com>
2019-10-29 14:57:49 +0900

    Change videoController.js with pageTitle

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,6 @@
 // globalRouter.js에서 활용할 함수들
-export const home = (req,res) => res.render("home"); // home is home.pug
+// res.render(템플리,템플릿추가할 정보담긴 객체)
+export const home = (req,res) => res.render("home", {pageTitle:"Home"}); // home is home.pug, 'pageTitle' is variable and "Home" is value. pageTitle이 home템플릿(home.pug)로 전달된다.
 export const search = (req,res) => res.render("search");
 export const upload = (req,res) => res.render("upload");
 export const videoDetail = (req,res) => res.render("videoDetail");
