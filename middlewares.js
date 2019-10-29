2f4d60a3790f10b3fbac4f2ec54a4f4f8a90e1ee
junblee <topguide82@gmail.com>
2019-10-29 14:40:21 +0900

    modify middlewares.js && header.pug

--- middlewares.js
+++ middlewares.js
@@ -1,3 +1,7 @@
+import routes from "./routes"; // add to object(routes.js)
+
 export const localsMiddleware = (req,res,next) => {
     res.locals.siteName = "JunTube";
+    res.locals.routes = routes; // add to object(routes.js)
+    next();
 }
