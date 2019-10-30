4c6b53c8aa08fd97e20ca8d79040ad917189ccb9
junblee <topguide82@gmail.com>
2019-10-30 16:26:11 +0900

    userController.js is changed res.send with res.render

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,11 @@
 // globalRouter.js에서 활용할 함수들
-export const join = (req,res) => res.send("Join");
-export const login = (req,res) => res.send("Login");
-export const logout = (req,res) => res.send("Logout");
+export const join = (req,res) => res.render("Join", { pageTitle : "user"});
+export const login = (req,res) => res.render("Login", { pageTitle : "Login"});
+export const logout = (req,res) => res.render("Logout", { pageTitle : "Logout"});
 
 // userRouter.js에서 활용할 함수들
-export const users = (req,res) => res.send("USERS!!");
-export const userDetail = (req,res) => res.send("userDetail!!");
-export const editProfile = (req,res) => res.send("editProfile!!");
-export const changePassword = (req,res) => res.send("changePassword!!");
+export const users = (req,res) => res.render("USERS!!", { pageTitle : "USERS"});
+export const userDetail = (req,res) => res.render("userDetail!!", { pageTitle : "userDetail"});
+export const editProfile = (req,res) => res.render("editProfile!!", { pageTitle : "editProfile"});
+export const changePassword = (req,res) => res.render("changePassword!!", { pageTitle : "changePassword"});
+
--- middlewares.js
+++ middlewares.js
@@ -1,5 +1,8 @@
+//하단의 코드들은 app.js에서 전역적으로 사용할 수 있도록 app.js파일의 코드에 위치해있다.
 import routes from "./routes"; // add to object(routes.js)
 
+
+//export함수 지정
 export const localsMiddleware = (req,res,next) => {
     res.locals.siteName = "JunTube";
     res.locals.routes = routes; // add to object(routes.js)
